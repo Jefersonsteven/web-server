2383f2115310c93ee950a37a3ff484e027a0f5cd
gfrey <gfreycode@gmail.com>
2024-02-29 09:18:28 -0500

    Update file paths and import statements

--- charts/main.py
+++ charts/main.py
@@ -2,7 +2,7 @@ from icecream import ic
 from chart import get_data_of_csv, create_chart
 
 def run():
-    labels, values, dataframe = get_data_of_csv('csv/world_population.csv', 'CCA3', 'World Population Percentage', ('Continent', 'South America'))
+    labels, values, dataframe = get_data_of_csv('assets/csv/world_population.csv', 'CCA3', 'World Population Percentage', ('Continent', 'South America'))
     create_chart(labels, values, 'South America Population Percentage', 'Countries', 'Population Percentage')
     ic(dataframe)
     
