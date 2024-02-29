e8ae05a019d21e4ef929e3ba7aa013e6895e0334
gfrey <gfreycode@gmail.com>
2024-02-29 10:08:20 -0500

    Update server.py with new endpoint and response class

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hola mundo 🌎"}
+
+@app.get("/data", response_class=HTMLResponse)
+def get_home():
+    return """
+        <h1>Hola mundo 🌎</h1>
+    """ 
